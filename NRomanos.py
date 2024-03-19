e0ec285f046d11dc76fe886b0ea91b1f2640afa1
ghuali <guaghuali@gmail.com>
2024-03-19 13:04:30 +0000

    Fixed roman2int

--- NRomanos.py
+++ NRomanos.py
@@ -3,18 +3,19 @@ class Romano:
         self.roman = roman
 
         
-romanos = {'I': 1, 'V': 5, 'X': 10,
+
+    @staticmethod
+    def roman2int(romval):
+        romanos = {'I': 1, 'V': 5, 'X': 10,
                'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        prev = 0
+        for letra in romval[::-1]:
+            valor = romanos[letra]
+            total += valor if valor >= prev else -valor
+            prev = valor
 
-def roman2int(romval):
-    total = 0
-    prev = 0
-    for letra in romval[::-1]:
-        valor = romanos[letra]
-        total += valor if valor >= prev else -valor
-        prev = valor
-
-    return total
+        return total
 
 def int2roman(valrom):
     total = ""
@@ -32,7 +33,7 @@ def int2roman(valrom):
 if __name__ == "__main__":
     tests = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "XIV", "CXC", "MIM","XXX"]
     for test in tests:
-        print(test, roman2int(test))
+        print(test, Romano.roman2int(test))
     testeo = [12,5,20,4]
     for testt in testeo:
         print(testt,int2roman(testt))
